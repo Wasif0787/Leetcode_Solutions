3aa490ca6bc1508db140d4885560951afb84ad51
Wasif Hussain <95441107+Wasif0787@users.noreply.github.com>
2023-02-19 22:42:24 +0530

    Update Solution.java

--- 896. Monotonic Array/Solution.java
+++ 896. Monotonic Array/Solution.java
@@ -1,17 +1,13 @@
 class Solution {
     public boolean isMonotonic(int[] A) {
-        boolean increase = false;
-        boolean decrease = false;
-        
-        for (int i = 0; i < A.length-1; i++) {
-            if (A[i+1] < A[i]) {
-                decrease = true;
-            }
-            if (A[i+1] > A[i]) {
-                increase = true;
-            }
+        boolean inc = true;
+        boolean dec = true;
+        for (int i=0; i<A.length-1; i++) {
+            if (A[i] > A[i+1])
+                inc = false;
+            if (A[i] < A[i+1])
+                dec = false;
         }
-        
-        return increase && decrease ? false : true;
+        return inc || dec;
     }
 }
