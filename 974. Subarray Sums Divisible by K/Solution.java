3d3d9e9483b8e1f954a33f9f8379eca2e96dccf9
Wasif Hussain <95441107+Wasif0787@users.noreply.github.com>
2023-02-19 22:06:47 +0530

    Update Solution.java

--- 974. Subarray Sums Divisible by K/Solution.java
+++ 974. Subarray Sums Divisible by K/Solution.java
@@ -1,28 +1,18 @@
 class Solution {
-    public String removeDuplicateLetters(String s) {
-        final int N = 26;
-        int[] count = new int[N];
-        boolean[] inQueue = new boolean[N];
-        Deque<Integer> dq = new LinkedList<>();
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
+    public int subarraysDivByK(int[] nums, int k) {
+        int mod[] = new int[k];
+        int n = nums.length;
+        Arrays.fill(mod, 0);
+        int cumSum = 0;
+        for (int i = 0; i < n; i++) {
+            cumSum += nums[i];
+            mod[((cumSum % k) + k) % k]++;
         }
-        for (int i = 0; i < s.length(); i++) {
-            int idx = (int)(s.charAt(i) - 'a');
-            count[idx]--;
-            if (inQueue[idx]) {
-                continue;
-            }
-            while (!dq.isEmpty() && dq.peekLast() > idx && count[dq.peekLast()] > 0) {
-                inQueue[dq.pollLast()] = false;
-            }
-            inQueue[idx] = true;
-            dq.offerLast(idx);
-        }
-        StringBuilder sb = new StringBuilder();
-        while (!dq.isEmpty()) {
-            sb.append((char)(dq.pollFirst() + 'a'));
-        }
-        return sb.toString();
+        int result = 0;
+        for (int i = 0; i < k; i++)
+            if (mod[i] > 1)
+                result += (mod[i] * (mod[i] - 1)) / 2;
+        result += mod[0];
+        return result;
     }
 }
